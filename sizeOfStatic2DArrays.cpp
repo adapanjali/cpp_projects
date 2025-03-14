73288bfce9d267e484b1534ece706b50b98626d3
Anjali <67096137+adapanjali@users.noreply.github.com>
2025-03-14 16:15:44 +0000

    addNewRowToArray

--- sizeOfStatic2DArrays.cpp
+++ sizeOfStatic2DArrays.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-    int arr[6][3] = {  // 3 rows, 4 columns
+    int arr[7][3] = {  // 3 rows, 4 columns
         {1, 2, 3},
         {5, 6, 7},
         {9, 10, 11}, 
         {12, 13, 14}, 
         {15, 16, 17},
-        (18, 19, 20)
+        {18, 19, 20}, 
+        {21, 22, 23}
     };
 
     cout << "Total size in bytes: " << sizeof(arr) << endl;
