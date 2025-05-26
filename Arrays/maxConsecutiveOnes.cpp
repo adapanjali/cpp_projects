297f9388f4e948827c6b037c52798f8c32ad4c37
Anjali <67096137+adapanjali@users.noreply.github.com>
2025-05-26 14:59:53 +0000

    changes

--- Arrays/maxConsecutiveOnes.cpp
+++ Arrays/maxConsecutiveOnes.cpp
@@ -1,14 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int ctr=0;
+        int ctr = 0;
         int maxCount = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
+            if (nums[i]) {
                 ctr++;
                 maxCount = max(maxCount, ctr);
-            } else if (nums[i] == 0){
+            } else {
                 ctr = 0;
             }
         }
@@ -16,3 +21,13 @@ public:
         return maxCount;
     }
 };
+
+int main() {
+    vector<int> nums = {1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1};
+    Solution sol;  // Create an object of the class
+    int result = sol.findMaxConsecutiveOnes(nums);  // Call the method
+
+    cout << "Maximum consecutive 1s: " << result << endl;
+
+    return 0;
+}
